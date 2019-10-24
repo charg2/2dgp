7296e59988307302de006cb5fc1fadb7befae94e
charg2 <charg2dang@gmail.com>
2019-10-24 23:32:33 +0900

    modify ConfigParser

--- 2DGP/termp/ConfigParser.py
+++ 2DGP/termp/ConfigParser.py
@@ -1,14 +1,14 @@
 
 class ConfigParser:
     map = {};
-    def __init__(self, config_name:str):
+    def __init__(self):
         pass;
     # for test
     #def print(self):
     #    f = ConfigParser.file.readline();
     #    print(f);
 
-    def parse(self):
+    def parse(self, config_name:str):
         file = open(config_name, "r", encoding = "UTF8");
         string = file.read();
         buffer = string.splitlines();
@@ -27,17 +27,18 @@ class ConfigParser:
         return ConfigParser.map.get(value_name);
 
     def get_bool(self, value_name:str):
-        return bool(ConfigParser.map.get(value_name));
+        return ConfigParser.map.get(value_name) == "true" or ConfigParser.map.get(value_name) == "True";
 
 if __name__ == "__main__":
-    parser = ConfigParser("config.ini");
+    parser = ConfigParser();
     #parser.print();
-    parser.parse();
+    parser.parse("config.ini");
     
     print(parser.get_int("speed"));
     print(parser.get_int("x"));
     print(parser.get_int("y"));
     print(parser.get_string("poetry"));
+    print(parser.get_bool("can_move"));
 
 
 

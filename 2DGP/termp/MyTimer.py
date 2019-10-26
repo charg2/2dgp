d1bc0d2c9fced1839971909e8d22a256b6c60e4c
charg2 <charg2dang@gmail.com>
2019-10-26 12:09:27 +0900

    timer

--- 2DGP/termp/MyTimer.py
+++ 2DGP/termp/MyTimer.py
@@ -14,32 +14,31 @@ class Timer:
         Timer.delta_time    = 0.0;
         return;
     
-
     def update():
         Timer.current_time = time.time();
-        Timer.delta_time   = Timer.current_time-Timer.prev_time;
+        Timer.delta_time   = Timer.current_time - Timer.prev_time;
         Timer.prev_time    = Timer.current_time;
         Timer.game_time    += Timer.delta_time;
         return;
 
-    def returnelapsedtime():
+    def get_elapsed_time():
         return Timer.delta_time;
 
     @staticmethod
-    def returnGameTime():
+    def get_game_time():
         return Timer.game_time;
 
     Initialize          = staticmethod(init);
     Update              = staticmethod(update);
-    ReturnElapsedTime   = staticmethod(returnelapsedtime);
+    GetElapsedTime      = staticmethod(get_elapsed_time);
    
 
-if __name__ == "__main__":
-    Timer.Initialize();
-    fTimer = 0.0;
-    while(True):
-        Timer.Update();
-        fTimer += Timer.ReturnElapsedTime();
-        if fTimer > 1.0:
-            print("delta = {0} , fTimer{1}".format(Timer.ReturnElapsedTime(),fTimer));
-            fTimer = 0.0;
+#if __name__ == "__main__":
+#    Timer.Initialize();
+#    fTimer = 0.0;
+#    while(True):
+#        Timer.Update();
+#        fTimer += Timer.GetElapsedTime();
+#        if fTimer > 1.0:
+#            print("delta = {0} , fTimer{1}".format(Timer.GetElapsedTime(),fTimer));
+#            fTimer = 0.0;
